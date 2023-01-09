a4301c6cd3d501b14eb997f3f7cb5d101cd1ffc8
redboard <ott.thomas68@gmail.com>
2023-01-09 10:57:35 +0100

    (+) base for api

--- api/src/index.js
+++ api/src/index.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const app = express()
 
-
-
-
-
-
-
-
 app.use(express.static('static'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.json());
 
 app.listen(8134, () => {
     console.log('Serveur à l écoute')
 })
-
