97986d2fb9c0f7fbd52e090e6f88938cd786f572
redboard <ott.thomas68@gmail.com>
2023-01-10 11:21:25 +0100

    (|) fix route for token->id

--- api/src/routes/user/user.js
+++ api/src/routes/user/user.js
@@ -8,17 +8,6 @@ function error_handling_body(req) {
     return true;
 }
 
-function get_user_id_with_token(req, res) {
-    try {
-        let decoded = jwt.verify(req.body.token, process.env.SECRET);
-        return (decoded.id);
-    } catch (err) {
-        console.log(err)
-        res.status(403).json({ msg: "Token is not valid" });
-    }
-    return (-1);
-}
-
 module.exports = async function(app, con) {
     app.get("/user", glob.verifyToken, async (req, res) => {
         if (!glob.verifyAuth(req, res, false)) {
@@ -35,16 +24,13 @@ module.exports = async function(app, con) {
         });
     });
 
-    app.get("/user/:token", glob.verifyToken, async (req, res) => {
-        if (!glob.verifyAuth(req, res, false)) {
+    app.get("/user/me", glob.verifyToken, async (req, res) => {
+        let token = req.token;
+        if (!glob.verifyAuth_without_id(req, res, true)) {
             !res.headersSent ? res.status(403).json({ msg: "Authorization denied" }) : 0;
             return;
         }
-        if (!error_handling_body(req)) {
-            res.status(400).json({ msg: "Bad parameter" });
-            return;
-        }
-        let token_id = get_user_id_with_token(req, res);
+        let token_id = glob.get_id_with_token(req, res);
         if (token_id === - 1)
             return;
         const queryString = (req.token === process.env.OTHER_APP_TOKEN) ? `*` : `id, username, email, discord_id, permission_id, created_at`;
