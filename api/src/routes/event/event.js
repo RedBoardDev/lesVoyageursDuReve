cd3f7d3dc63212140d442d90a4b67d86a2246cbf
redboard <ott.thomas68@gmail.com>
2023-01-11 11:33:42 +0100

    (+) delete event route

--- api/src/routes/event/event.js
+++ api/src/routes/event/event.js
@@ -198,7 +198,7 @@ module.exports = async function(app, con) {
                             if (err2) {
                                 res.status(500).json({ msg: "Internal server error" });
                             } else
-                                res.status(200).json( {msg: "event change"} );
+                                res.status(200).json( {msg: "event changed"} );
                         });
                     } else
                         res.status(403).json({ msg: "Authorization denied" });
@@ -208,30 +208,37 @@ module.exports = async function(app, con) {
 
     });
 
-    // app.delete("/game/:id", glob.verifyToken, async (req, res) => {
-    //     if (!glob.is_num(req.params.id)) {
-    //         res.status(400).json({ msg: "Bad parameter" });
-    //         return;
-    //     }
-    //     if (!glob.verifyAuth_without_id(req, res, true)) {
-    //         !res.headersSent ? res.status(403).json({ msg: "Authorization denied" }) : 0;
-    //         return;
-    //     }
-    //     let token_id = glob.get_id_with_token(req, res);
-    //     if (token_id === -1)
-    //         res.status(403).json({ msg: "Authorization denied" })
-    //     con.query(`SELECT permission_id FROM users WHERE id ="${token_id}";`, function (err, rows) {
-    //         if (err)
-    //             res.status(500).json({ msg: "Internal server error" });
-    //         else if (rows[0]['permission_id'] === 2) {
-    //             con.query(`DELETE FROM games WHERE id = "${req.params.id}";`, function (err2, result) {
-    //                 if (err2)
-    //                     res.status(500).json({ msg: "Internal server error" });
-    //                 else
-    //                     res.status(200).json( {msg: "games removed"} );
-    //             });
-    //         } else
-    //             res.status(403).json({ msg: "Authorization denied" });
-    //     });
-    // });
+    app.delete("/event/:id", glob.verifyToken, async (req, res) => {
+        if (!glob.is_num(req.params.id)) {
+            res.status(400).json({ msg: "Bad parameter" });
+            return;
+        }
+        if (!glob.verifyAuth_without_id(req, res, true)) {
+            !res.headersSent ? res.status(403).json({ msg: "Authorization denied" }) : 0;
+            return;
+        }
+        let token_id = glob.get_id_with_token(req, res);
+        if (token_id === -1)
+            res.status(403).json({ msg: "Authorization denied" });
+        con.query(`SELECT permission_id FROM users WHERE id ="${token_id}";`, function (err, rows1) {
+            if (err)
+                res.status(500).json({ msg: "Internal server error" });
+            else {
+                con.query(`SELECT admin_user_id FROM events WHERE id ="${req.params.id}";`, function (err1, rows) {
+                    if (err1)
+                        res.status(500).json({ msg: "Internal server error" });
+                    else if (rows1[0]['permission_id'] === 2 || (rows1[0]['permission_id'] >= 1 && parseInt(token_id) === rows[0]['admin_user_id'])) {
+                        con.query(`DELETE FROM events WHERE id = "${req.params.id}";`, function (err2, result) {
+                            if (err2) {
+                                res.status(500).json({ msg: "Internal server error" });
+                            } else
+                                res.status(200).json( {msg: "event removed"} );
+                        });
+                    } else
+                        res.status(403).json({ msg: "Authorization denied" });
+                });
+            }
+        });
+
+    });
 }
