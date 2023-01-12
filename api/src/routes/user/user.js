0e8a84a0741f44088769503e7dd057bc317463d9
redboard <ott.thomas68@gmail.com>
2023-01-12 16:46:09 +0100

    (|) permission handlings for /user

--- api/src/routes/user/user.js
+++ api/src/routes/user/user.js
@@ -1,13 +1,27 @@
 const glob = require('../../global');
 const jwt = require('jsonwebtoken');
 
+function get_id_user_route(req) {
+    if (req.token === process.env.OTHER_APP_TOKEN)
+        return (-2);
+    try {
+        let decoded = jwt.verify(req.token, process.env.SECRET);
+        return (decoded.id === -1 ? -1: 0);
+    } catch (err) {
+        return (-1);
+    }
+}
+
 module.exports = async function(app, con) {
-    app.get("/user", glob.verifyToken, async (req, res) => {
-        if (!glob.verifyAuth(req, res, false)) {
-            !res.headersSent ? res.status(403).json({ msg: "Authorization denied" }) : 0;
-            return;
-        }
-        const queryString = (req.token === process.env.OTHER_APP_TOKEN) ? `*` : `id, username, email, discord_id, permission_id, discord_username, discord_avatar, created_at`;
+    app.get("/user", async (req, res) => {
+        const rsp = get_id_user_route(req);
+        var queryString = '';
+        if (rsp === -2)
+            queryString = `*`;
+        else if (rsp === 0)
+            queryString = `id, username, discord_id, permission_id, discord_username, discord_avatar, created_at`;
+        else
+        queryString = `id, username, email, discord_id, discord_username, discord_avatar, created_at`;
         con.query(`SELECT ${queryString} FROM users;`, function (err, rows) {
             if (err)
                 res.status(500).json({ msg: "Internal server error" });
