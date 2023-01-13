70a5031b2ae08ac0d62d0188972335c93f1f1618
david <david.benistant@epitech.eu>
2023-01-13 09:14:23 +0100

    admin.js

--- api/static/js/admin.js
+++ api/static/js/admin.js
@@ -8,6 +8,7 @@ function loadPage ()
                 loadGameType()
                 loadGame()
                 loadPlace()
+                loadUsers()
             } else {
                 window.location.href = "/"
             }
@@ -21,6 +22,34 @@ function loadPage ()
 var GameType
 var Game
 var Place
+var Users
+
+function loadUsers()
+{
+    $.ajax({
+        type: "GET",
+        url: "/user",
+        contentType: "application/json; charset=utf-8",
+        dataType :"json",
+        headers: {
+            "Authorization":"Bearer " + sessionStorage.getItem("lvdrToken")
+        },
+        success: function(result) {
+            Users = result
+            let selector = document.getElementById("UserSelect")
+
+            for (let i = 0; i < result.length; ++i) {
+                let option = document.createElement("option")
+                option.setAttribute("value" , result[i].id)
+                option.textContent = result[i].username
+                selector.append(option)
+            }
+        },
+        error: function(e){
+            console.log(e)
+        }
+    });
+}
 
 function loadGameType()
 {
@@ -201,6 +230,44 @@ function createPlace ()
             window.location.reload()
         },
         error: function(e){
+            console.log(e)
+        }
+    });
+}
+
+function changeUser()
+{
+    let selector = document.getElementById("UserSelect").value
+
+    if (selector == -1) {
+        document.getElementById("UserUsername").value = ""
+        document.getElementById("UserEmail").value = ""
+    } else {
+        let User = Users.find(element => element.id == selector)
+        document.getElementById("UserUsername").value = User.username
+        document.getElementById("UserEmail").value = User.email
+    }
+}
+
+function deleteUser()
+{
+    let value = document.getElementById("UserSelect").value
+
+    if (value == -1)
+        return
+    $.ajax({
+        type: "DELETE",
+        url: "/user/id/" + value,
+        contentType: "application/json; charset=utf-8",
+        dataType :"json",
+        headers: {
+            "Authorization":"Bearer " + sessionStorage.getItem("lvdrToken")
+        },
+        success: function(result) {
+            window.location.reload()
+        },
+        error: function(e){
+            err("error")
             console.log(e)
         }
     });
