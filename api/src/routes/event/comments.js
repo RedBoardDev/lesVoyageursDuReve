69c6602e3ef98cb865619bc8965da79d3ef85888
RedBoard <ott.thomas68@gmail.com>
2023-02-03 00:16:02 +0100

    (|) convert mysql to dynamoDB for comments

--- api/src/routes/event/comments.js
+++ api/src/routes/event/comments.js
@@ -1,4 +1,5 @@
-const glob = require('../../global');
+const DB_Userfunction = require('../../DB/users');
+const DB_Commentsfunction = require('../../DB/comments');
 const tokenVerify = require('../../tokenVerify');
 
 function error_handling_values(req) {
@@ -22,59 +23,51 @@ module.exports = async function(app, con) {
             return;
         }
         let token_id = tokenVerify.get_id_with_token(req, res);
-        if (token_id === -1)
+        if (token_id === -1) {
             res.status(403).json({ msg: "Authorization denied" });
-                con.query(`INSERT INTO comments(event_id, user_id, message) VALUES("${req.body["event_id"]}", "${token_id}", "${req.body["message"]}")`, function (err2, result) {
-                    if (err2)
-                        res.status(500).json({ msg: "Internal server error" });
-                    else
-                        res.status(200).json( {msg: "comment added"} );
-                });
-    });
-
-    app.get("/comment/:event_id", async (req, res) => {
-        if (!glob.is_num(req.params.event_id)) {
-            res.status(400).json({ msg: "Bad parameter" });
             return;
         }
-        con.query(`SELECT * FROM comments WHERE event_id = "${req.params.event_id}";`, function (err, rows) {
+        DB_Commentsfunction.createComment({"event_id": req.body["event_id"], "user_id": token_id, "message": req.body["message"]}, con, function(err) {
             if (err)
                 res.status(500).json({ msg: "Internal server error" });
             else
-                res.send(rows);
+                res.status(200).json( {msg: "comment added"} );
+        });
+    });
+
+    app.get("/comment/:event_id", async (req, res) => {
+        DB_Commentsfunction.getCommentById(req.params.event_id, con, function(err, data) {
+            if (err)
+                res.status(500).json({ msg: "Internal server error" });
+            else
+                res.send(data);
         });
     });
 
     app.delete("/comment/:comment_id", tokenVerify.verifyToken, async (req, res) => {
-        if (!glob.is_num(req.params.comment_id)) {
-            res.status(400).json({ msg: "Bad parameter" });
-            return;
-        }
         if (!tokenVerify.verifyAuth_without_id(req, res, true)) {
             !res.headersSent ? res.status(403).json({ msg: "Authorization denied" }) : 0;
             return;
         }
         let token_id = tokenVerify.get_id_with_token(req, res);
-        if (token_id === -1)
+        if (token_id === -1) {
             res.status(403).json({ msg: "Authorization denied" });
-        con.query(`SELECT permission_id FROM users WHERE id ="${token_id}";`, function (err, rows1) {
+            return;
+        }
+        DB_Userfunction.getUserById(['permission_id'], token_id, con, function(err, data) {
             if (err)
                 res.status(500).json({ msg: "Internal server error" });
             else {
-                con.query(`SELECT id, user_id FROM comments WHERE id ="${req.params.comment_id}";`, function (err1, rows2) {
-                    if (err1 || rows2.length === 0)
+                DB_Commentsfunction.getCommentById(req.params.comment_id, con, function (err1, data1) {
+                    if (err1 || data1 === undefined)
                         res.status(500).json({ msg: "Internal server error" });
-                    else if ((token_id === -2 || rows1[0]['permission_id'] === 2) || parseInt(token_id) === rows2[0]['user_id']) {
-
-                        con.query(`DELETE FROM comments WHERE id = "${rows2[0]['id']}";`, function (err, result) {
-                            if (err)
+                    else if ((token_id === -2 || data['permission_id'] === 2) || token_id === data1['user_id']) {
+                        DB_Commentsfunction.deleteComment(data1['id'], con, function (err2) {
+                            if (err2)
                                 res.status(500).json({ msg: "Internal server error" });
-                            else if (rows1[0] && result.affectedRows !== 0) {
+                            else
                                 res.status(200).json({ msg: `Successfully deleted record number: ${req.params.event_id}` });
-                            } else
-                                res.sendStatus(404);
                         });
-
                     } else
                         res.status(403).json({ msg: "Authorization denied" });
                 });
